02e53e40706ac6ff66c8ac2d749b0419db0e27fe
kirakira0 <kiratoal0@gmail.com>
2020-01-30 00:14:48 -0800

    added styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
 import TodoList from './TodoList'
 import TodoEntryForm from './TodoEntryForm';
 
 export default function App() {
   const [todos, setTodos] = useState([
-    {id: 1, content: 'Meet Masao for Udon'},
-    {id: 2, content: 'Fix up these horrible React notes'},
+    {id: 1, content: 'math webwork'},
+    {id: 2, content: 'h theo hmwk'},
   ]);
   function deleteTodo(id) {
     setTodos(todos => todos.filter(todo => todo.id !== id));
@@ -14,10 +17,12 @@ export default function App() {
     setTodos(todos => [...todos, todo]);
   }
   return (
-    <div className="App">
-      <h1>Things To Do</h1>
-      <TodoList todos={todos} deleter={deleteTodo} />
-      <TodoEntryForm adder={addTodo} />
-    </div>
+    <Container maxWidth="sm" style={{marginTop: 20}}>
+      <Paper style={{padding: 20, backgroundColor: '#fffff3'}}>
+        <Typography variant="h3" align="center">Things To Do</Typography>
+        <TodoList todos={todos} deleter={deleteTodo}/>
+        <TodoEntryForm adder={addTodo} />
+      </Paper>
+    </Container>
   );
 }
